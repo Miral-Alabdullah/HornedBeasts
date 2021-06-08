bf2b5ea441cca35a121cde57fc5cbbece4212f80
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-06-08 06:48:57 +0300

    Finshing lab02

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # HornedBeasts
+
+## 1. Number and name of feature: **Feature #1: Display images**
+
+## 2. Estimate of time needed to complete: 30 minutes
+
+## 3. Start time: 4:05
+
+## 4. Finish time: 5:25
+
+## 5. Actual time needed to complete: <30 minutes
+
+<hr>
+<br>
+<hr>
+
+## 1. Number and name of feature: **Feature #2: Allow users to vote for their favorite beast**
+
+## 2. Estimate of time needed to complete: 65 minutes
+
+## 3. Start time: 5:40
+
+## 4. Finish time: 6:45
+
+## 5. Actual time needed to complete: <30 minutes
--- lab01/src/componenets/HonredBeasts.js
+++ lab01/src/componenets/HonredBeasts.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 class HornedBeasts extends React.Component {
+	constructor (props) {
+		super(props)
+		this.state = {
+			vote: 0
+		}
+	}
+
+	requestVoting = () => {
+		let tempVote = this.state.vote;
+		this.setState({
+			vote: tempVote+=1
+		})
+	}
+
   render() {
 	return(
 		<div>
 			<div>
 			<h1>{this.props.title}</h1>
-			<img src={this.props.imageUrl} title={this.props.title}/>
+			<img src={this.props.imageUrl} alt={this.props.title} onClick={this.requestVoting}/>
 			<p>{this.props.description}</p>
+			<p>Vote: {this.state.vote}</p>
+
 			</div>
 		</div>
 	);
